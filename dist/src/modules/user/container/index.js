1583ff580d03b9a72ed98579babc3bdceb046cc8
wonedd <vinmarv@hotmail.com>
2022-10-31 17:16:43 -0300

    rebuild

--- dist/src/modules/user/container/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const tsyringe_1 = require("tsyringe");
-const UsersRepository_1 = require("../repositories/implementations/UsersRepository");
-tsyringe_1.container.registerSingleton('UsersRepository', UsersRepository_1.UsersRepository);

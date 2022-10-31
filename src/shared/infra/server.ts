166e5237d48d797e19e0b8665e22119be176a25d
wonedd <vinmarv@hotmail.com>
2022-10-31 17:07:27 -0300

    server listener

--- src/shared/infra/server.ts
+++ src/shared/infra/server.ts
@@ -2,10 +2,4 @@
 /* eslint-disable func-names */
 import { app } from './app';
 
-app.listen(process.env.PORT || 3333, function () {
-  console.log(
-    'Express server listening on port %d in %s mode',
-    this.address().port,
-    app.settings.env,
-  );
-});
+app.listen(process.env.PORT || 3333);

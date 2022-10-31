637e3f42e0ed3eda6b70faaf1256f6a9712b295d
wonedd <vinmarv@hotmail.com>
2022-10-31 16:29:37 -0300

    rebuild

--- dist/shared/infra/server.js
+++ dist/shared/infra/server.js
@@ -1,7 +1,8 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+/* eslint-disable no-console */
+/* eslint-disable func-names */
 const app_1 = require("./app");
-app_1.app.listen(3333, () => {
-    // eslint-disable-next-line no-console
-    console.log('Server is running on port 3333');
+app_1.app.listen(process.env.PORT || 3333, function () {
+    console.log('Express server listening on port %d in %s mode', this.address().port, app_1.app.settings.env);
 });

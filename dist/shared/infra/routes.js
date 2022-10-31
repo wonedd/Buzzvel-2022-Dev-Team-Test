d6ae19ce361b77d14459f0e0b49e4232cb5b8beb
wonedd <vinmarv@hotmail.com>
2022-10-31 17:07:42 -0300

    route changes

--- dist/shared/infra/routes.js
+++ dist/shared/infra/routes.js
@@ -4,4 +4,4 @@ exports.routes = void 0;
 const users_routes_1 = require("../../modules/user/infra/users.routes");
 const express_1 = require("express");
 exports.routes = (0, express_1.Router)();
-exports.routes.use('/', users_routes_1.usersRouter);
+exports.routes.use('/users', users_routes_1.usersRouter);
--- dist/shared/infra/server.js
+++ dist/shared/infra/server.js
@@ -3,6 +3,4 @@ Object.defineProperty(exports, "__esModule", { value: true });
 /* eslint-disable no-console */
 /* eslint-disable func-names */
 const app_1 = require("./app");
-app_1.app.listen(process.env.PORT || 3333, function () {
-    console.log('Express server listening on port %d in %s mode', this.address().port, app_1.app.settings.env);
-});
+app_1.app.listen(process.env.PORT || 3333);

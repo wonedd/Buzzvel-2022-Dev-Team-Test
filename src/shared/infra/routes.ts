873dca097c725c0e2ced2376a53666110ee25584
wonedd <vinmarv@hotmail.com>
2022-11-01 14:46:48 -0300

    route changes

--- src/shared/infra/routes.ts
+++ src/shared/infra/routes.ts
@@ -1,15 +1,10 @@
 import { usersRouter } from '@modules/user/infra/users.routes';
-import { AuthenticateUserController } from '@modules/user/useCases/authenticateUser/AuthenticateUserController';
 import { Router } from 'express';
 
 export const routes = Router();
-
-routes.use('/users', usersRouter);
-
 const authenticateRoutes = Router();
 
-const authenticateUserController = new AuthenticateUserController();
-
-authenticateRoutes.post('/sessions', authenticateUserController.handle);
+routes.use('/', usersRouter);
+routes.use(authenticateRoutes);
 
 export { authenticateRoutes };

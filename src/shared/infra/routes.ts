518fdc0d2e43c1ebba867f7b451d48ae7d1aa9f9
wonedd <vinmarv@hotmail.com>
2022-11-01 16:37:20 -0300

    session route fix

--- src/shared/infra/routes.ts
+++ src/shared/infra/routes.ts
@@ -1,10 +1,8 @@
+import { authenticateRoutes } from '@modules/user/infra/authenticate.routes';
 import { usersRouter } from '@modules/user/infra/users.routes';
 import { Router } from 'express';
 
 export const routes = Router();
-const authenticateRoutes = Router();
 
 routes.use('/', usersRouter);
 routes.use('/sessions', authenticateRoutes);
-
-export { authenticateRoutes };
